255df0e3ea0bb01a2eb49e4a8e0902e5cb54b233
jm <cothdlsrks3@naver.com>
2024-05-13 12:57:19 +0900

    중근식

--- java01/src/java01/MyClass.java
+++ java01/src/java01/MyClass.java
@@ -3,7 +3,7 @@ package java01;
 public class MyClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("중근식25강상재^20^방어구^10^아이템레벨^1643^");
 
 	}
 
